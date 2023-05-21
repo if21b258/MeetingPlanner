1f82b683788b25e90fed517c8442a5fed2d32cba
if21b258 <if21b258@technikum-wien.at>
2023-05-21 13:15:07 +0200

    command and context setup

---
+++ source/Model/TourModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.source.Model
+{
+    class Tour
+    {
+        public int TourId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Trasportation { get; set; }
+        public string Distance { get; set; }
+        public string EstimatedTime { get; set; }
+        public string RouteInformation { get; set; }
+        
+    }
+}
--- source/UI/ViewModel/TourListViewModel.cs
+++ source/UI/ViewModel/TourListViewModel.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using TourPlanner.source.BL;
+using TourPlanner.source.UI.View;
 
 namespace source.UI.ViewModel
 {
-    class TourListViewModel
+    public class TourListViewModel
     {
-        public TourListViewModel() {
-        
+        public ICommand AddTourCommand { get; set; }
+
+        public TourListViewModel()
+        {
+            AddTourCommand = new RelayCommand(AddTour);
+        }
+
+        private void AddTour(object obj)
+        {
+            AddTour addtour = new AddTour();
+            addtour.ShowDialog();
         }
     }
 }
