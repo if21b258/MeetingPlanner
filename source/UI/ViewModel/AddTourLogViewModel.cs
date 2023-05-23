60472db5c91d80ad5e02f12f8459612b271976b9
Riemenschneider Felix <Felix.Riemenschneider@at.bosch.com>
2023-05-23 14:56:39 +0200

    ViewModels created

--- source/UI/ViewModel/AddTourLogViewModel.cs
+++ source/UI/ViewModel/AddTourLogViewModel.cs
@@ -1,8 +1,130 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace TourPlanner.source.UI.ViewModel
 {
-	public Class1()
-	{
-	}
+    public class AddTourLogViewModel // vlt noch von etwas erben
+    {
+        //private TourItem _currentTour;
+
+        private ICommand _addTourLogCommand;
+        private ICommand _addTourLogCommand;
+
+        public Action CancelAddTourLog { get; set; }
+
+        private string _date;
+
+        private string _time;
+
+        private string _comment;
+
+        private int _difficulty;
+
+        private string _totalTime;
+
+        private int _rating;
+
+        public ICommand AddTourLogCommand => _addTourLogCommand ??= new RelayCommand(AddTour);
+        public ICommand CancelTourLogCommand => _cancelAddTourLogCommand ??= new RelayCommand(CancelAddTourLog);
+
+        /*public AddTourLogViewModel(TourItem currentTour)
+        {
+            _currentTour = currentTour;
+        }*/
+
+        public string Date
+        {
+            get { return _date; }
+
+            set
+            {
+                _date = value;
+
+                //Errorhandling fehlt noch 
+            }
+
+        }
+
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                _comment = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        public int Difficulty
+        {
+            get { return _difficulty; }
+            set
+            {
+                _difficulty = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        public string TotalTime
+        {
+            get { return _totalTime; }
+            set
+            {
+                _totalTime = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        public int Rating
+        {
+            get { return _rating; }
+            set
+            {
+                _rating = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        /*private void AddTourLog(object commandParameter)
+        {
+            if (_tourLogDistance != 0 && _tourLogDuration != 0 && !String.IsNullOrEmpty(_tourLogReport)
+                && !String.IsNullOrEmpty(_tourLogWeather) && _tourLogTemperature != 0)
+            {
+                AddedTourLog?.Invoke(this, new TourLogItem(_currentTour.Name, DateTime.Now, _tourLogDistance, _tourLogDuration, _tourLogReport, _tourLogRating, _tourLogFuelUsed, _tourLogWeather, _tourLogTemperature, _tourLogEffort));
+            }
+            else
+            {
+                throw new ArgumentException("Please fill in all fields");
+            }
+        }*/
+
+
+        private void CancelWindow(object commandParameter)
+        {
+            if (commandParameter != null)
+            {
+                (commandParameter as Window).Close();
+            }
+        }
+
+
+    }
 }
--- source/UI/ViewModel/AddTourViewModel.cs
+++ source/UI/ViewModel/AddTourViewModel.cs
@@ -1,8 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+
+namespace TourPlanner.source.UI.ViewModel
 {
-	public Class1()
-	{
-	}
+    public class AddTourViewModel // vlt noch von etwas erben
+    {
+
+        private ICommand _addTourCommand;
+        private ICommand _addTourCommand;
+
+        public Action CancelAddTour { get; set; }
+
+        private string _name;
+
+        private string _description;
+
+        private string _origin;
+
+        private int _destination;
+
+        private string _transportType;
+
+
+        //public event EventHandler<TourAttribute> AddedTour;
+
+        //public ObservableCollection<TourAttribute> Tours { get; set; }
+
+
+        public ICommand AddTourCommand => _addTourCommand ??= new RelayCommand(AddTour);
+        public ICommand CancelTourCommand => _cancelAddTourCommand ??= new RelayCommand(CancelAddTour);
+
+        /*public AddTourLogViewModel(TourAttribute currentTour)
+        {
+            _currentTour = currentTour;
+        }*/
+
+        /*public AddTourViewModel(ObservableCollection<TourAttribute> tours)
+        {
+            this.Tours = tours;
+        }*/
+
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                _name = value;
+
+                //Errorhandling fehlt noch 
+            }
+
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        public string Origin
+        {
+            get { return _origin; }
+            set
+            {
+                _origin = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        public int Destination
+        {
+            get { return _destination; }
+            set
+            {
+                _destination = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+        public string TransportType
+        {
+            get { return _transportType; }
+            set
+            {
+                _transportType = value;
+
+                //Errorhandling fehlt noch 
+            }
+        }
+
+
+        /*private void AddTour(object commandParameter)
+        {
+            if (!String.IsNullOrEmpty(_tourName) && !String.IsNullOrEmpty(_tourDescription) && !String.IsNullOrEmpty(_tourFrom)
+                && !String.IsNullOrEmpty(_tourTo) && !String.IsNullOrEmpty(_tourTransportMode))
+            {
+                AddedTour?.Invoke(this, new TourItem(_tourName, _tourDescription, _tourFrom, _tourTo, _tourTransportMode));
+            }
+            else
+            {
+                throw new ArgumentException("Please fill in all fields");
+            }
+        }+/
+
+
+        private void CancelWindow(object commandParameter)
+        {
+            if (commandParameter != null)
+            {
+                (commandParameter as Window).Close();
+            }
+        }
+
+
+    }
 }
