15cde7abda39b29e782a5a916dd4e9395cef88e5
if21b258 <if21b258@technikum-wien.at>
2023-06-14 12:39:49 +0200

    edit tour fix

--- source/UI/ViewModel/EditTourViewModel.cs
+++ source/UI/ViewModel/EditTourViewModel.cs
@@ -13,16 +13,9 @@ namespace TourPlannerUI.ViewModel
     public class EditTourViewModel : BaseViewModel
     {
         private TourListViewModel _tourListViewModel;
-
+        private TourModel _selectedTour;
         public ICommand EditTourCommand { get; set; }
 
-        private string? _name;
-        private string? _origin;
-        private string? _destination;
-        private string? _transportType;
-        private string? _description;
-
-        private TourModel _selectedTour;
 
         public EditTourViewModel(TourListViewModel tourListViewModel)
         {
@@ -36,7 +29,7 @@ namespace TourPlannerUI.ViewModel
             get { return _selectedTour.Name; }
             set
             {
-                _name = value;
+                _selectedTour.Name = value;
 
                 //Errorhandling fehlt noch 
             }
@@ -47,7 +40,7 @@ namespace TourPlannerUI.ViewModel
             get { return _selectedTour.Origin; }
             set
             {
-                _origin = value;
+                _selectedTour.Origin = value;
 
                 //Errorhandling fehlt noch 
             }
@@ -58,7 +51,7 @@ namespace TourPlannerUI.ViewModel
             get { return _selectedTour.Destination; }
             set
             {
-                _destination = value;
+                _selectedTour.Destination = value;
 
                 //Errorhandling fehlt noch 
             }
@@ -69,7 +62,7 @@ namespace TourPlannerUI.ViewModel
             get { return _selectedTour.TransportType; }
             set
             {
-                _transportType = value;
+                _selectedTour.TransportType = value;
 
                 //Errorhandling fehlt noch 
             }
@@ -80,7 +73,7 @@ namespace TourPlannerUI.ViewModel
             get { return _selectedTour.Description; }
             set
             {
-                _description = value;
+                _selectedTour.Description = value;
 
                 //Errorhandling fehlt noch 
             }
@@ -88,7 +81,7 @@ namespace TourPlannerUI.ViewModel
 
         private void EditTour(object commandParameter)
         {
-            if (!String.IsNullOrEmpty(_name) && !String.IsNullOrEmpty(_origin) && !String.IsNullOrEmpty(_destination)
+/*            if (!String.IsNullOrEmpty(_name) && !String.IsNullOrEmpty(_origin) && !String.IsNullOrEmpty(_destination)
                 && !String.IsNullOrEmpty(_transportType) && !String.IsNullOrEmpty(_description))
             {
                 _tourListViewModel.TourList.Add(new TourModel(_name, _origin, _destination, _transportType, _description));
@@ -96,7 +89,7 @@ namespace TourPlannerUI.ViewModel
             else
             {
                 throw new ArgumentException("Please fill in all fields");
-            }
+            }*/
         }
     }
 }
