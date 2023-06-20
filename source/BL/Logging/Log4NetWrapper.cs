b3ce2f8415f13460ad39fbd6fce4d5e3a355f786
if21b258 <if21b258@technikum-wien.at>
2023-06-20 17:05:27 +0200

    logging setup

---
+++ source/BL/Logging/Log4NetWrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlannerBL.Logging
+{
+    public class Log4NetWrapper : ILoggerWrapper
+    {
+        private log4net.ILog _logger;
+
+        private Log4NetWrapper(log4net.ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public static Log4NetWrapper CreateLogger(string configPath)
+        {
+            if (!File.Exists(configPath))
+            {
+                throw new ArgumentException("Does not exist.", nameof(configPath));
+            }
+
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(configPath));
+
+
+            var logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            return new Log4NetWrapper(logger);
+        }
+
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Info(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            _logger.Fatal(message);
+        }
+    }
+}
---
+++ source/BL/Logging/LoggerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlannerBL.Logging
+{
+    public static class LoggerFactory
+    {
+        public static ILoggerWrapper GetLogger()
+        {
+            return Log4NetWrapper.CreateLogger("./log4net.config");
+        }
+    }
+}
