9ecb0be1d28c43fe00422b9c8ef788eba1ce8884
Riemenschneider Felix <Felix.Riemenschneider@at.bosch.com>
2023-06-26 16:26:00 +0200

    Enums created

--- source/Model/TourModel.cs
+++ source/Model/TourModel.cs
@@ -15,30 +15,28 @@ namespace TourPlannerModel
         public string Name { get; set; }
         public string Origin { get; set; }
         public string Destination { get; set; }
-        public string TransportType { get; set; }
+        public Transport TransportType { get; set; }
         public string Description { get; set; }
         public float Distance { get; set; }
         public float EstimatedTime { get; set; }
 
         public TourModel() { }
 
-        public TourModel(string name, string origin, string destination, string transportType, string description, float distance, float estimatedTime)
+        public TourModel(string name, string origin, string destination, string description, float distance, float estimatedTime)
         {
             Name = name;
             Origin = origin;
             Destination = destination;
-            TransportType = transportType;
             Description = description;
             Distance = distance;
             EstimatedTime = estimatedTime;
         }
 
-        public TourModel(string name, string origin, string destination, string transportType, string description)
+        public TourModel(string name, string origin, string destination, string description)
         {
             Name = name;
             Origin = origin;
             Destination = destination;
-            TransportType = transportType;
             Description = description;
             Distance = 0;
             EstimatedTime = 0;
---
+++ source/Model/Transport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TourPlannerModel
+{
+    public enum Transport
+    {
+        [Display(Name = "Car")]
+        Fastest,
+
+        [Display(Name = "By Foot")]
+        Pedestrian,
+
+        [Display(Name = "Bicycle")]
+        Bicycle
+    }
+}
